41609d834fb00bb4a2154d61ad95ae5eeea8df97
Klaus Tockloth <klaus.tockloth@googlemail.com>
2026-06-03 09:27:13 +0200

    freebsd, netbsd, openbsd removed

--- build-uploads.sh
+++ build-uploads.sh
@@ -7,6 +7,7 @@
 # Releases:
 # - v1.0.0 - 2025-03-11: initial release
 # - v1.1.0 - 2026-02-19: Windows ARM-32 support removed, ARM-64 added
+# - v1.2.0 - 2026-06-03: freebsd, netbsd, openbsd removed
 # ------------------------------------
 
 # set -o xtrace
@@ -20,21 +21,9 @@ mkdir ./uploads
 tar -cvzf ./uploads/macos-amd64_gem-pro.tar.gz ./binaries/darwin-amd64/gem-pro
 tar -cvzf ./uploads/macos-arm64_gem-pro.tar.gz ./binaries/darwin-arm64/gem-pro
 
-# uploads 'freebsd'
-tar -cvzf ./uploads/freebsd-amd64_gem-pro.tar.gz ./binaries/freebsd-amd64/gem-pro
-tar -cvzf ./uploads/freebsd-arm64_gem-pro.tar.gz ./binaries/freebsd-arm64/gem-pro
-
 # uploads 'linux'
 tar -cvzf ./uploads/linux-amd64_gem-pro.tar.gz ./binaries/linux-amd64/gem-pro
 tar -cvzf ./uploads/linux-arm64_gem-pro.tar.gz ./binaries/linux-arm64/gem-pro
-
-# uploads 'netbsd'
-tar -cvzf ./uploads/netbsd-amd64_gem-pro.tar.gz ./binaries/netbsd-amd64/gem-pro
-tar -cvzf ./uploads/netbsd-arm64_gem-pro.tar.gz ./binaries/netbsd-arm64/gem-pro
-
-# uploads 'openbsd'
-tar -cvzf ./uploads/openbsd-amd64_gem-pro.tar.gz ./binaries/openbsd-amd64/gem-pro
-tar -cvzf ./uploads/openbsd-arm64_gem-pro.tar.gz ./binaries/openbsd-arm64/gem-pro
 
 # uploads 'windows'
 zip ./uploads/windows-amd64_gem-pro.zip ./binaries/windows-amd64/gem-pro.exe
